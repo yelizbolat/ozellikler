9332959c6ed903c30c7dd239efdb4bf000ac9417
bolat <bolat@YELIZ>
2023-11-28 10:54:02 +0300

    Proje dosyası ekle.

---
+++ ozellikler/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ozellikler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    public class Ucgen
+    {
+        int a;
+        int b;
+        int c;
+
+        public int A
+        {
+            get { return a; }
+            set
+            {
+                if (value <= 0)
+                    Console.WriteLine("hatali bilgi");
+                else
+                    a = value;
+            }
+        }
+        public int B
+        {
+            get { return b; }
+            set
+            {
+                if (value <= 0)
+                    Console.WriteLine("hatali bilgi");
+                else
+                    b = value;
+            }
+        }
+        public int C
+        {
+            get { return b; }
+            set
+            {
+                if (value <= 0)
+                    Console.WriteLine("hatali bilgi");
+                else
+                    c= value;
+            }
+        }
+    }
+}
